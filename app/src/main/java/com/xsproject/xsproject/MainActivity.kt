ab28bcb1862ec8264d153145acace58e1b6b5cd2
夏沐尧 <xiamuyaodqx@gmail.com>
2017-10-21 14:24:39 +0800

    提交插件 不用findviewbyid

--- app/src/main/java/com/xsproject/xsproject/MainActivity.kt
+++ app/src/main/java/com/xsproject/xsproject/MainActivity.kt
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity
  *==========================
  */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
